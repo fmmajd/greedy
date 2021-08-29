87ad8a9ca4aecc8d1d3b5ce4bb4a70ff4f38ba2d
Fatemeh Majd <fmmajd452@gmail.com>
2021-08-29 19:37:11 +0430

    added factory for Product

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 
@@ -13,6 +14,8 @@ use Illuminate\Database\Eloquent\Relations\HasMany;
  */
 class Product extends Model
 {
+    use HasFactory;
+
     protected $table = 'products';
 
     protected $fillable = ['name', 'price'];
