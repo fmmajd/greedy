58120740cb47854d5e1b98914a6e2db912d23c40
Fatemeh Majd <fmmajd452@gmail.com>
2021-08-29 16:30:09 +0430

    added test for correct inventory in http request

--- tests/Feature/App/Http/Controllers/WareHouseControllerTest.php
+++ tests/Feature/App/Http/Controllers/WareHouseControllerTest.php
@@ -3,16 +3,36 @@
 namespace Tests\Feature\App\Http\Controllers;
 
 use Illuminate\Http\UploadedFile;
+use Illuminate\Testing\TestResponse;
 use Tests\TestCase;
 
 class WareHouseControllerTest extends TestCase
 {
-    /**
-     * A basic feature test example.
-     *
-     * @return void
-     */
     public function test_warehouse_route_should_return_ok()
+    {
+        $response = $this->getResponse();
+
+        $response->assertStatus(200);
+    }
+
+    public function test_warehouse_route_should_return_correct_inventory()
+    {
+        $response = $this->getResponse();
+
+        $response->assertJson([
+            'data' => [
+                'profit' => 2000,
+                'products' => [
+                    [
+                        'name' => 'Dining Chair',
+                        'quantity' => 2,
+                    ]
+                ],
+            ],
+        ]);
+    }
+
+    private function getResponse(): TestResponse
     {
         $articlesPath = __DIR__ . '/stubs/articles.json';
         $articles = new UploadedFile(
@@ -32,11 +52,9 @@ class WareHouseControllerTest extends TestCase
             true
         );
 
-        $response = $this->post('/api/warehouse', [
+        return $this->post('/api/warehouse', [
             'articles' => $articles,
             'products' => $products,
         ]);
-
-        $response->assertStatus(200);
     }
 }
