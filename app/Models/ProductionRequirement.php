4f444faeb6e19273993f2bae9ed44e1055c85b68
Fatemeh Majd <fmmajd452@gmail.com>
2021-08-29 19:37:30 +0430

    added factory for ProductionRequirement

--- app/Models/ProductionRequirement.php
+++ app/Models/ProductionRequirement.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
@@ -11,6 +12,8 @@ use Illuminate\Database\Eloquent\Relations\BelongsTo;
  */
 class ProductionRequirement extends Model
 {
+    use HasFactory;
+
     protected $table = 'production_requirements';
 
     protected $fillable = ['product_id', 'article_ref_id', 'amount'];
